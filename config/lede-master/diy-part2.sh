64856e8faf3bc2395dcdc8eb392272c863a75938
297460389 <297460389@qq.com>
2024-09-28 16:34:09 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -28,8 +28,10 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # ------------------------------- Other started -------------------------------
 #
-# Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+#####Add a feed source
+
+echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
+
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
