466e750f0b7b380be945cdf416a33d797fc8dac8
297460389 <297460389@qq.com>
2024-10-02 20:38:58 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -32,7 +32,7 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
 
-#echo 'src-git openwrt-packages https://github.com/dansir118/openwrt-packages' >>feeds.conf.default
+echo 'src-git openwrt-packages https://github.com/dansir118/openwrt-packages' >>feeds.conf.default
 
 
 # Fix runc version error
